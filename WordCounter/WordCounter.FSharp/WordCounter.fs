650a9b60fc8ce4eecf8acd7acfe92bda2318dd7e
Jan Bajer <jan.bajer@gmail.com>
2024-05-22 22:30:34 +0200

    Add F# implementation

---
+++ WordCounter/WordCounter.FSharp/WordCounter.fs
@@ -0,0 +1,14 @@
+﻿namespace WordCounter.FSharp
+
+open System
+open WordCounter.Common
+
+type FSharpWordCounter() =
+    interface IWordCounter with
+        member this.CountAndSortWords(input: string) =
+            input.Split(null) |>
+            Seq.filter(fun s -> not <| String.IsNullOrWhiteSpace s) |>
+            Seq.groupBy(id) |>
+            Seq.map(fun (key, seq) -> new WordCount(key, Seq.length seq)) |>
+            Seq.sortBy(fun w -> w.Word) |>
+            Seq.sortByDescending(fun w -> w.Count)
---
+++ WordCounter/WordCounter.Tests/FSharpTests.cs
@@ -0,0 +1,35 @@
+﻿using WordCounter.Common;
+using WordCounter.FSharp;
+using WordCounter.Tests.Utils;
+
+namespace WordCounter.Tests;
+
+public class FSharpTests
+{
+    private IWordCounter _wordCounter;
+
+    [SetUp]
+    public void Setup()
+    {
+        _wordCounter = new FSharpWordCounter();
+    }
+
+    [TestCase("Input1", "Output1")]
+    [TestCase("Input2", "Output2")]
+    [TestCase("Input3", "Output3")]
+    [TestCase("Input4", "Output4")]
+    [TestCase("Input5", "Output5")]
+    [TestCase("Input6", "Output6")]
+    [TestCase("Input7", "Output7")]
+    [TestCase("Input8", "Output8")]
+    [TestCase("Input9", "Output9")]
+    [TestCase("Input10", "Output10")]
+    public void Test(string inputName, string outputName)
+    {
+        var (input, expectedOutput) = TestUtils.LoadCase(inputName, outputName);
+
+        var actual = _wordCounter.CountAndSortWords(input);
+
+        Assert.That(actual, Is.EqualTo(expectedOutput));
+    }
+}
